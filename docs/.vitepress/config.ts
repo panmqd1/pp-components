5eb9ec2cb50b17517ea87559da738c81216baf00
panmqd1 <36785224+panmqd1@users.noreply.github.com>
2023-10-19 11:11:02 +0800

    publish: test

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,38 +1,47 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "PP-Components",
   description: "components by pmq",
+  // 项目目录
+  base: "/pp-components/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/components/border-wrapper' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/components/border-wrapper" },
     ],
 
     sidebar: [
       {
-        text: 'Guide',
-        items: [
-          { text: 'Update Logs', link: '/update-logs' }
-        ]
+        text: "Guide",
+        items: [{ text: "Update Logs", link: "/update-logs" }],
       },
       {
-        text: 'Components',
+        text: "Components",
         items: [
-          { text: '边框容器 BorderWrapper', link: '/components/border-wrapper' },
-          { text: '图表空态 ChartEmpty', link: '/components/chart-empty' },
-          { text: '简易表格 CustomTable', link: '/components/custom-table' },
-          { text: '日期月份切换 DateMonthPicker', link: '/components/date-month-picker' },
-          { text: '缩略标签 EllipticTags', link: '/components/elliptic-tags' },
-          { text: '自定义加载 Loading', link: '/components/loading' },
-          { text: '图标 ReIcon', link: '/components/reicon' },
-          { text: '缩放盒子 ScreenBox', link: '/components/screen-box' },
-          { text: '表格空态 TableEmpty', link: '/components/table-empty' },
-          { text: '星期月份切换 WeekMonthPicker', link: '/components/week-month-picker' },
-        ]
-      }
+          {
+            text: "边框容器 BorderWrapper",
+            link: "/components/border-wrapper",
+          },
+          { text: "图表空态 ChartEmpty", link: "/components/chart-empty" },
+          { text: "简易表格 CustomTable", link: "/components/custom-table" },
+          {
+            text: "日期月份切换 DateMonthPicker",
+            link: "/components/date-month-picker",
+          },
+          { text: "缩略标签 EllipticTags", link: "/components/elliptic-tags" },
+          { text: "自定义加载 Loading", link: "/components/loading" },
+          { text: "图标 ReIcon", link: "/components/reicon" },
+          { text: "缩放盒子 ScreenBox", link: "/components/screen-box" },
+          { text: "表格空态 TableEmpty", link: "/components/table-empty" },
+          {
+            text: "星期月份切换 WeekMonthPicker",
+            link: "/components/week-month-picker",
+          },
+        ],
+      },
       // {
       //   text: 'Examples',
       //   items: [
@@ -42,7 +51,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
