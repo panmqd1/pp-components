6d3e89b286515bb28a87b5a2c9c1b5b148a47282
panmqd1 <36785224+panmqd1@users.noreply.github.com>
2023-11-13 17:56:04 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <Test />
-  <!-- <WaterFall /> -->
 </template>
 <script setup lang="ts">
 import Test from '/@/test/index.vue'
-// import WaterFall from '/@/test/waterfall.vue'
 </script>
 <style scoped></style>
